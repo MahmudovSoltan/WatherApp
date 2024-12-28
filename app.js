9f23ee3e7583e4769c713dfe3209a2e79f3c161e
Soltan <mahmudovsoltan477@gmail.com>
2024-12-28 09:48:53 +0400

    add

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const url = "http://api.openweathermap.org/data/2.5/";
+const url = "https://api.openweathermap.org/data/2.5/";
 const apikey = "c5eab4cb159224e46e9b1675b6dd41bc";
 const input = document.querySelector("#input");
 const title = document.querySelector("h2");
 const wather_dec = document.querySelector("#wather_dec");
 const humidy = document.querySelector("#humidy");
 const wind = document.querySelector("#wind");
-const image = document.querySelector("img")
+const image = document.querySelector("img");
+
 const setQuery = (e) => {
   if (e.keyCode == 13) {
     getResult(input.value);
@@ -13,8 +14,13 @@ const setQuery = (e) => {
 };
 
 const getResult = (inputValue) => {
-  fetch( `${url}weather?q=${inputValue}&appid=${apikey}&units=metric&lang=az`)
-    .then((response) => response.json())
+  fetch(`${url}weather?q=${inputValue}&appid=${apikey}&units=metric&lang=az`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => displayResult(data))
     .catch((err) => console.error("Error fetching data:", err));
 };
@@ -23,22 +29,24 @@ const displayResult = (data) => {
   console.log(data);
   title.innerHTML = data.name;
   wather_dec.innerHTML = `${Math.round(data.main.temp)}°C`;
-  humidy.innerHTML = ` <i class="fa-solid fa-water"></i>${data.main.humidity}`;
-  wind.innerHTML = `<i class="fa-solid fa-wind"></i>${data.wind.speed}`;
-  if(data.weather[0].main == "Clouds"){
-      image.src = "./images/cloud.png"
-   } else if(data.weather[0].main =="Rain"){
-     image.src = "./images/rain.png"
-   }
-    else if(data.weather[0].main =="Snow"){
-     image.src = "./images/snow.png"
-   }
-    else if(data.weather[0].main =="Sunny"){
-     image.src = "./images/clear.png"
-   }
-    else if(data.weather[0].main =="Windy"){
-     image.src = "./images/mist.png"
-   }
+  humidy.innerHTML = `<i class="fa-solid fa-water"></i> ${data.main.humidity}%`;
+  wind.innerHTML = `<i class="fa-solid fa-wind"></i> ${data.wind.speed} m/s`;
+
+  const weatherMain = data.weather[0].main.toLowerCase();
+  if (weatherMain.includes("cloud")) {
+    image.src = "./images/cloud.png";
+  } else if (weatherMain.includes("rain")) {
+    image.src = "./images/rain.png";
+  } else if (weatherMain.includes("snow")) {
+    image.src = "./images/snow.png";
+  } else if (weatherMain.includes("clear")) {
+    image.src = "./images/clear.png";
+  } else if (weatherMain.includes("mist")) {
+    image.src = "./images/mist.png";
+  } else {
+    image.src = "./images/default.png";
+  }
+
   input.value = "";
 };
 
